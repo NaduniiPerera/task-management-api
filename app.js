907e39587ecd3995f6a611ad3c15e63f02ed3ab3
NaduniiPerera <it23218680@my.sliit.lk>
2026-05-30 15:07:49 +0530

    Add CORS support for frontend integration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const taskRoutes = require("./routes/taskRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -6,18 +8,17 @@ const logger = require("./middleware/logger");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(logger);
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Task Management API is running",
-  });
-});
 
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/categories", categoryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Task Management API is running");
+});
 
 app.use((req, res) => {
   res.status(404).json({
